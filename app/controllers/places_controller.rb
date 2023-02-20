d58a3ad8de845db0e8c6ec948ee5aea8f51f3c9f
YANGHONG-KELLOGG <yang.hong@kellogg.northwestern.edu>
2023-02-20 01:45:51 +0000

    places done

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,30 @@
+class PlacesController < ApplicationController
+
+    def index
+        @places = Place.all 
+    end
+
+    def show
+        @place = Place.find_by({ "id" => params["id"] })       
+        #@contacts = Contact.where({ "company_id" => @company["id"] })
+     end
+   
+     def new
+        @place = Place.new
+     end 
+
+     def create
+        @place = Place.new
+        @place["name"] = params["place"]["name"]
+        @place.save
+        redirect_to "/places"
+      end
+
+
+
+
+
+
+
+
+end
